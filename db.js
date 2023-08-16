5b2bbf88d2857ab707ca77b1b4c28e4d7bfc4897
Maleena <maleena.sohail@gmail.com>
2023-08-16 16:08:51 +0530

    ch

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI="mongodb://localhost:27017"
-
+const mongoURI="mongodb+srv://maleena:masabtank@cluster0.frhmatj.mongodb.net/?retryWrites=true&w=majority"
+//mongodb://localhost:27017
 const connectToMongo = () =>{
     mongoose.connect(mongoURI,()=>{
         console.log("connected to mongo successfully");
